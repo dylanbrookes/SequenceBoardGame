e98fb739df91ad6e45b124ecf8bd4a634b405647
dylanbrookes <33613317+dylanbrookes@users.noreply.github.com>
2019-02-10 02:28:11 -0500

    modified data file structure

--- data.js
+++ data.js
@@ -2,420 +2,620 @@ let data =
 [
 	[
 		{
+			"column": 1,
+			"row": 1,
 			"value": null,
 			"type": "wildCard"
 		},
 		{
+			"column": 1,
+			"row": 2,
 			"value": 8,
 			"type": "spade"
 		},
 		{
+			"column": 1,
+			"row": 3,
 			"value": 7,
 			"type": "spade"
 		},
 		{
+			"column": 1,
+			"row": 4,
 			"value": 6,
 			"type": "spade"
 		},
 		{
+			"column": 1,
+			"row": 5,
 			"value": 5,
 			"type": "spade"
 		},
 		{
+			"column": 1,
+			"row": 6,
 			"value": 4,
 			"type": "spade"
 		},
 		{
+			"column": 1,
+			"row": 7,
 			"value": 3,
 			"type": "spade"
 		},
 		{
+			"column": 1,
+			"row": 8,
 			"value": 2,
 			"type": "spade"
 		},
 		{
+			"column": 1,
+			"row": 9,
 			"value": 1,
 			"type": "spade"
 		},
 		{
+			"column": 1,
+			"row": 10,
 			"value": null,
 			"type": "wildCard"
 		}
 	],
 	[
 		{
+			"column": 2,
+			"row": 1,
 			"value": 10,
 			"type": "spade"
 		},
 		{
+			"column": 2,
+			"row": 2,
 			"value": 10,
 			"type": "heart"
 		},
 		{
+			"column": 2,
+			"row": 3,
 			"value": 12,
 			"type": "heart"
 		},
 		{
+			"column": 2,
+			"row": 4,
 			"value": 13,
 			"type": "heart"
 		},
 		{
+			"column": 2,
+			"row": 5,
 			"value": 1,
 			"type": "heart"
 		},
 		{
+			"column": 2,
+			"row": 6,
 			"value": 2,
 			"type": "club"
 		},
 		{
+			"column": 2,
+			"row": 7,
 			"value": 3,
 			"type": "club"
 		},
 		{
+			"column": 2,
+			"row": 8,
 			"value": 4,
 			"type": "club"
 		},
 		{
+			"column": 2,
+			"row": 9,
 			"value": 5,
 			"type": "club"
 		},
 		{
+			"column": 2,
+			"row": 10,
 			"value": 6,
 			"type": "club"
 		}
 	],
 	[
 		{
+			"column": 3,
+			"row": 1,
 			"value": 12,
 			"type": "spade"
 		},
 		{
+			"column": 3,
+			"row": 2,
 			"value": 9,
 			"type": "heart"
 		},
 		{
+			"column": 3,
+			"row": 3,
 			"value": 7,
 			"type": "diamond"
 		},
 		{
+			"column": 3,
+			"row": 4,
 			"value": 6,
 			"type": "diamond"
 		},
 		{
+			"column": 3,
+			"row": 5,
 			"value": 5,
 			"type": "diamond"
 		},
 		{
+			"column": 3,
+			"row": 6,
 			"value": 4,
 			"type": "diamond"
 		},
 		{
+			"column": 3,
+			"row": 7,
 			"value": 3,
 			"type": "diamond"
 		},
 		{
+			"column": 3,
+			"row": 8,
 			"value": 2,
 			"type": "diamond"
 		},
 		{
+			"column": 3,
+			"row": 9,
 			"value": 1,
 			"type": "spade"
 		},
 		{
+			"column": 3,
+			"row": 10,
 			"value": 7,
 			"type": "club"
 		}
 	],
 	[
 		{
+			"column": 4,
+			"row": 1,
 			"value": 13,
 			"type": "spade"
 		},
 		{
+			"column": 4,
+			"row": 2,
 			"value": 8,
 			"type": "heart"
 		},
 		{
+			"column": 4,
+			"row": 3,
 			"value": 8,
 			"type": "diamond"
 		},
 		{
+			"column": 4,
+			"row": 4,
 			"value": 2,
 			"type": "club"
 		},
 		{
+			"column": 4,
+			"row": 5,
 			"value": 3,
 			"type": "club"
 		},
 		{
+			"column": 4,
+			"row": 6,
 			"value": 4,
 			"type": "club"
 		},
 		{
+			"column": 4,
+			"row": 7,
 			"value": 5,
 			"type": "club"
 		},
 		{
+			"column": 4,
+			"row": 8,
 			"value": 6,
 			"type": "club"
 		},
 		{
+			"column": 4,
+			"row": 9,
 			"value": 13,
 			"type": "spade"
 		},
 		{
+			"column": 4,
+			"row": 10,
 			"value": 8,
 			"type": "club"
 		}
 	],
 	[
 		{
+			"column": 5,
+			"row": 1,
 			"value": 1,
 			"type": "spade"
 		},
 		{
+			"column": 5,
+			"row": 2,
 			"value": 7,
 			"type": "heart"
 		},
 		{
+			"column": 5,
+			"row": 3,
 			"value": 9,
 			"type": "diamond"
 		},
 		{
+			"column": 5,
+			"row": 4,
 			"value": 1,
 			"type": "heart"
 		},
 		{
+			"column": 5,
+			"row": 5,
 			"value": 4,
 			"type": "heart"
 		},
 		{
+			"column": 5,
+			"row": 6,
 			"value": 5,
 			"type": "heart"
 		},
 		{
+			"column": 5,
+			"row": 7,
 			"value": 6,
 			"type": "heart"
 		},
 		{
+			"column": 5,
+			"row": 8,
 			"value": 7,
 			"type": "club"
 		},
 		{
+			"column": 5,
+			"row": 9,
 			"value": 12,
 			"type": "spade"
 		},
 		{
+			"column": 5,
+			"row": 10,
 			"value": 9,
 			"type": "club"
 		}
 	],
 	[
 		{
+			"column": 6,
+			"row": 1,
 			"value": 2,
 			"type": "diamond"
 		},
 		{
+			"column": 6,
+			"row": 2,
 			"value": 6,
 			"type": "heart"
 		},
 		{
+			"column": 6,
+			"row": 3,
 			"value": 10,
 			"type": "diamond"
 		},
 		{
+			"column": 6,
+			"row": 4,
 			"value": 12,
 			"type": "heart"
 		},
 		{
+			"column": 6,
+			"row": 5,
 			"value": 3,
 			"type": "heart"
 		},
 		{
+			"column": 6,
+			"row": 6,
 			"value": 2,
 			"type": "heart"
 		},
 		{
+			"column": 6,
+			"row": 7,
 			"value": 7,
 			"type": "heart"
 		},
 		{
+			"column": 6,
+			"row": 8,
 			"value": 8,
 			"type": "club"
 		},
 		{
+			"column": 6,
+			"row": 9,
 			"value": 10,
 			"type": "spade"
 		},
 		{
+			"column": 6,
+			"row": 10,
 			"value": 10,
 			"type": "club"
 		}
 	],
 	[
 		{
+			"column": 7,
+			"row": 1,
 			"value": 3,
 			"type": "diamond"
 		},
 		{
+			"column": 7,
+			"row": 2,
 			"value": 5,
 			"type": "heart"
 		},
 		{
+			"column": 7,
+			"row": 3,
 			"value": 12,
 			"type": "diamond"
 		},
 		{
+			"column": 7,
+			"row": 4,
 			"value": 12,
 			"type": "heart"
 		},
 		{
+			"column": 7,
+			"row": 5,
 			"value": 10,
 			"type": "heart"
 		},
 		{
+			"column": 7,
+			"row": 6,
 			"value": 9,
 			"type": "heart"
 		},
 		{
+			"column": 7,
+			"row": 7,
 			"value": 8,
 			"type": "heart"
 		},
 		{
+			"column": 7,
+			"row": 8,
 			"value": 9,
 			"type": "club"
 		},
 		{
+			"column": 7,
+			"row": 9,
 			"value": 9,
 			"type": "spade"
 		},
 		{
+			"column": 7,
+			"row": 10,
 			"value": 12,
 			"type": "club"
 		}
 	],
 	[
 		{
+			"column": 8,
+			"row": 1,
 			"value": 4,
 			"type": "diamond"
 		},
 		{
+			"column": 8,
+			"row": 2,
 			"value": 4,
 			"type": "heart"
 		},
 		{
+			"column": 8,
+			"row": 3,
 			"value": 13,
 			"type": "diamond"
 		},
 		{
+			"column": 8,
+			"row": 4,
 			"value": 1,
 			"type": "diamond"
 		},
 		{
+			"column": 8,
+			"row": 5,
 			"value": 1,
 			"type": "club"
 		},
 		{
+			"column": 8,
+			"row": 6,
 			"value": 13,
 			"type": "club"
 		},
 		{
+			"column": 8,
+			"row": 7,
 			"value": 12,
 			"type": "club"
 		},
 		{
+			"column": 8,
+			"row": 8,
 			"value": 10,
 			"type": "club"
 		},
 		{
+			"column": 8,
+			"row": 9,
 			"value": 8,
 			"type": "spade"
 		},
 		{
+			"column": 8,
+			"row": 10,
 			"value": 13,
 			"type": "club"
 		}
 	],
 	[
 		{
+			"column": 9,
+			"row": 1,
 			"value": 5,
 			"type": "diamond"
 		},
 		{
+			"column": 9,
+			"row": 2,
 			"value": 3,
 			"type": "heart"
 		},
 		{
+			"column": 9,
+			"row": 3,
 			"value": 2,
 			"type": "heart"
 		},
 		{
+			"column": 9,
+			"row": 4,
 			"value": 2,
 			"type": "spade"
 		},
 		{
+			"column": 9,
+			"row": 5,
 			"value": 3,
 			"type": "spade"
 		},
 		{
+			"column": 9,
+			"row": 6,
 			"value": 4,
 			"type": "spade"
 		},
 		{
+			"column": 9,
+			"row": 7,
 			"value": 5,
 			"type": "spade"
 		},
 		{
+			"column": 9,
+			"row": 8,
 			"value": 6,
 			"type": "spade"
 		},
 		{
+			"column": 9,
+			"row": 9,
 			"value": 7,
 			"type": "spade"
 		},
 		{
+			"column": 9,
+			"row": 10,
 			"value": 1,
 			"type": "club"
 		}
 	],
 	[
 		{
+			"column": 10,
+			"row": 1,
 			"value": null,
 			"type": "wildCard"
 		},
 		{
+			"column": 10,
+			"row": 2,
 			"value": 6,
 			"type": "diamond"
 		},
 		{
+			"column": 10,
+			"row": 3,
 			"value": 7,
 			"type": "diamond"
 		},
 		{
+			"column": 10,
+			"row": 4,
 			"value": 8,
 			"type": "diamond"
 		},
 		{
+			"column": 10,
+			"row": 5,
 			"value": 9,
 			"type": "diamond"
 		},
 		{
+			"column": 10,
+			"row": 6,
 			"value": 10,
 			"type": "diamond"
 		},
 		{
+			"column": 10,
+			"row": 7,
 			"value": 12,
 			"type": "diamond"
 		},
 		{
+			"column": 10,
+			"row": 8,
 			"value": 13,
 			"type": "diamond"
 		},
 		{
+			"column": 10,
+			"row": 9,
 			"value": 1,
 			"type": "diamond"
 		},
 		{
+			"column": 10,
+			"row": 10,
 			"value": null,
 			"type": "wildCard"
 		}
--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-data.forEach( function (row) {
-    console.log(row);
-});
+for (var i = 0; i < data.length; i++) {
+    for (var x = 0; x < data.length; x++) {
+        // console.log(data[i][x]);
+        if (data[i][x]['value'] === null)
+        console.log(data[i][x]);
+    }
+}

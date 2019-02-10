340e9530530e51613a6a862bd19261e8f073f3fb
dylanbrookes <33613317+dylanbrookes@users.noreply.github.com>
2019-02-10 01:56:41 -0500

    finished data file

--- data.js
+++ data.js
@@ -7,7 +7,7 @@ let data =
 		},
 		"position2": {
 			"value": 8,
-			"cardType": "spade"
+			"type": "spade"
 		},
 		"position3": {
 			"value": 7,
@@ -49,7 +49,7 @@ let data =
 		},
 		"position2": {
 			"value": 10,
-			"cardType": "heart"
+			"type": "heart"
 		},
 		"position3": {
 			"value": 12,
@@ -65,23 +65,317 @@ let data =
 		},
 		"position6": {
 			"value": 2,
-			"type": "clubs"
+			"type": "club"
 		},
 		"position7": {
 			"value": 3,
-			"type": "clubs"
+			"type": "club"
 		},
 		"position8": {
 			"value": 4,
-			"type": "clubs"
+			"type": "club"
 		},
 		"position9": {
 			"value": 5,
-			"type": "clubs"
+			"type": "club"
 		},
 		"position10": {
 			"value": 6,
-			"type": "clubs"
+			"type": "club"
+		}
+	},
+	{
+		"position1": {
+			"value": 12,
+			"type": "spade"
+		},
+		"position2": {
+			"value": 9,
+			"type": "heart"
+		},
+		"position3": {
+			"value": 7,
+			"type": "diamond"
+		},
+		"position4": {
+			"value": 6,
+			"type": "diamond"
+		},
+		"position5": {
+			"value": 5,
+			"type": "diamond"
+		},
+		"position6": {
+			"value": 4,
+			"type": "diamond"
+		},
+		"position7": {
+			"value": 3,
+			"type": "diamond"
+		},
+		"position8": {
+			"value": 2,
+			"type": "diamond"
+		},
+		"position9": {
+			"value": 1,
+			"type": "spade"
+		},
+		"position10": {
+			"value": 7,
+			"type": "club"
+		}
+	},
+	{
+		"position1": {
+			"value": 13,
+			"type": "spade"
+		},
+		"position2": {
+			"value": 8,
+			"type": "heart"
+		},
+		"position3": {
+			"value": 8,
+			"type": "diamond"
+		},
+		"position4": {
+			"value": 2,
+			"type": "club"
+		},
+		"position5": {
+			"value": 3,
+			"type": "club"
+		},
+		"position6": {
+			"value": 4,
+			"type": "club"
+		},
+		"position7": {
+			"value": 5,
+			"type": "club"
+		},
+		"position8": {
+			"value": 6,
+			"type": "club"
+		},
+		"position9": {
+			"value": 13,
+			"type": "spade"
+		},
+		"position10": {
+			"value": 8,
+			"type": "club"
+		}
+	},
+	{
+		"position1": {
+			"value": 1,
+			"type": "spade"
+		},
+		"position2": {
+			"value": 7,
+			"type": "heart"
+		},
+		"position3": {
+			"value": 9,
+			"type": "diamond"
+		},
+		"position4": {
+			"value": 1,
+			"type": "heart"
+		},
+		"position5": {
+			"value": 4,
+			"type": "heart"
+		},
+		"position6": {
+			"value": 5,
+			"type": "heart"
+		},
+		"position7": {
+			"value": 6,
+			"type": "heart"
+		},
+		"position8": {
+			"value": 7,
+			"type": "club"
+		},
+		"position9": {
+			"value": 12,
+			"type": "spade"
+		},
+		"position10": {
+			"value": 9,
+			"type": "club"
+		}
+	},
+	{
+		"position1": {
+			"value": 2,
+			"type": "diamond"
+		},
+		"position2": {
+			"value": 6,
+			"type": "heart"
+		},
+		"position3": {
+			"value": 10,
+			"type": "diamond"
+		},
+		"position4": {
+			"value": 12,
+			"type": "heart"
+		},
+		"position5": {
+			"value": 3,
+			"type": "heart"
+		},
+		"position6": {
+			"value": 2,
+			"type": "heart"
+		},
+		"position7": {
+			"value": 7,
+			"type": "heart"
+		},
+		"position8": {
+			"value": 8,
+			"type": "club"
+		},
+		"position9": {
+			"value": 10,
+			"type": "spade"
+		},
+		"position10": {
+			"value": 10,
+			"type": "club"
+		}
+	},
+	{
+		"position1": {
+			"value": 3,
+			"type": "diamond"
+		},
+		"position2": {
+			"value": 5,
+			"type": "heart"
+		},
+		"position3": {
+			"value": 12,
+			"type": "diamond"
+		},
+		"position4": {
+			"value": 12,
+			"type": "heart"
+		},
+		"position5": {
+			"value": 10,
+			"type": "heart"
+		},
+		"position6": {
+			"value": 9,
+			"type": "heart"
+		},
+		"position7": {
+			"value": 8,
+			"type": "heart"
+		},
+		"position8": {
+			"value": 9,
+			"type": "club"
+		},
+		"position9": {
+			"value": 9,
+			"type": "spade"
+		},
+		"position10": {
+			"value": 12,
+			"type": "club"
+		}
+	},
+	{
+		"position1": {
+			"value": 4,
+			"type": "diamond"
+		},
+		"position2": {
+			"value": 4,
+			"type": "heart"
+		},
+		"position3": {
+			"value": 13,
+			"type": "diamond"
+		},
+		"position4": {
+			"value": 1,
+			"type": "diamond"
+		},
+		"position5": {
+			"value": 1,
+			"type": "club"
+		},
+		"position6": {
+			"value": 13,
+			"type": "club"
+		},
+		"position7": {
+			"value": 12,
+			"type": "club"
+		},
+		"position8": {
+			"value": 10,
+			"type": "club"
+		},
+		"position9": {
+			"value": 8,
+			"type": "spade"
+		},
+		"position10": {
+			"value": 13,
+			"type": "club"
+		}
+	},
+	{
+		"position1": {
+			"value": 5,
+			"type": "diamond"
+		},
+		"position2": {
+			"value": 3,
+			"type": "heart"
+		},
+		"position3": {
+			"value": 2,
+			"type": "heart"
+		},
+		"position4": {
+			"value": 2,
+			"type": "spade"
+		},
+		"position5": {
+			"value": 3,
+			"type": "spade"
+		},
+		"position6": {
+			"value": 4,
+			"type": "spade"
+		},
+		"position7": {
+			"value": 5,
+			"type": "spade"
+		},
+		"position8": {
+			"value": 6,
+			"type": "spade"
+		},
+		"position9": {
+			"value": 7,
+			"type": "spade"
+		},
+		"position10": {
+			"value": 1,
+			"type": "club"
 		}
 	},
 	{
@@ -90,330 +384,36 @@ let data =
 			"type": "wildCard"
 		},
 		"position2": {
-			"value": 8,
-			"cardType": "spade"
+			"value": 6,
+			"type": "diamond"
 		},
 		"position3": {
 			"value": 7,
-			"type": "spade"
+			"type": "diamond"
 		},
 		"position4": {
-			"value": 6,
-			"type": "spade"
-		},
-		"position5": {
-			"value": 5,
-			"type": "spade"
-		},
-		"position6": {
-			"value": 4,
-			"type": "spade"
-		},
-		"position7": {
-			"value": 3,
-			"type": "spade"
-		},
-		"position8": {
-			"value": 2,
-			"type": "spade"
-		},
-		"position9": {
-			"value": 1,
-			"type": "spade"
-		},
-		"position10": {
-			"value": null,
-			"type": "wildCard"
-		}
-	},
-	{
-		"position1": {
-			"value": null,
-			"type": "wildCard"
-		},
-		"position2": {
 			"value": 8,
-			"cardType": "spade"
-		},
-		"position3": {
-			"value": 7,
-			"type": "spade"
-		},
-		"position4": {
-			"value": 6,
-			"type": "spade"
+			"type": "diamond"
 		},
 		"position5": {
-			"value": 5,
-			"type": "spade"
+			"value": 9,
+			"type": "diamond"
 		},
 		"position6": {
-			"value": 4,
-			"type": "spade"
+			"value": 10,
+			"type": "diamond"
 		},
 		"position7": {
-			"value": 3,
-			"type": "spade"
+			"value": 12,
+			"type": "diamond"
 		},
 		"position8": {
-			"value": 2,
-			"type": "spade"
+			"value": 13,
+			"type": "diamond"
 		},
 		"position9": {
 			"value": 1,
-			"type": "spade"
-		},
-		"position10": {
-			"value": null,
-			"type": "wildCard"
-		}
-	},
-	{
-		"position1": {
-			"value": null,
-			"type": "wildCard"
-		},
-		"position2": {
-			"value": 8,
-			"cardType": "spade"
-		},
-		"position3": {
-			"value": 7,
-			"type": "spade"
-		},
-		"position4": {
-			"value": 6,
-			"type": "spade"
-		},
-		"position5": {
-			"value": 5,
-			"type": "spade"
-		},
-		"position6": {
-			"value": 4,
-			"type": "spade"
-		},
-		"position7": {
-			"value": 3,
-			"type": "spade"
-		},
-		"position8": {
-			"value": 2,
-			"type": "spade"
-		},
-		"position9": {
-			"value": 1,
-			"type": "spade"
-		},
-		"position10": {
-			"value": null,
-			"type": "wildCard"
-		}
-	},
-	{
-		"position1": {
-			"value": null,
-			"type": "wildCard"
-		},
-		"position2": {
-			"value": 8,
-			"cardType": "spade"
-		},
-		"position3": {
-			"value": 7,
-			"type": "spade"
-		},
-		"position4": {
-			"value": 6,
-			"type": "spade"
-		},
-		"position5": {
-			"value": 5,
-			"type": "spade"
-		},
-		"position6": {
-			"value": 4,
-			"type": "spade"
-		},
-		"position7": {
-			"value": 3,
-			"type": "spade"
-		},
-		"position8": {
-			"value": 2,
-			"type": "spade"
-		},
-		"position9": {
-			"value": 1,
-			"type": "spade"
-		},
-		"position10": {
-			"value": null,
-			"type": "wildCard"
-		}
-	},
-	{
-		"position1": {
-			"value": null,
-			"type": "wildCard"
-		},
-		"position2": {
-			"value": 8,
-			"cardType": "spade"
-		},
-		"position3": {
-			"value": 7,
-			"type": "spade"
-		},
-		"position4": {
-			"value": 6,
-			"type": "spade"
-		},
-		"position5": {
-			"value": 5,
-			"type": "spade"
-		},
-		"position6": {
-			"value": 4,
-			"type": "spade"
-		},
-		"position7": {
-			"value": 3,
-			"type": "spade"
-		},
-		"position8": {
-			"value": 2,
-			"type": "spade"
-		},
-		"position9": {
-			"value": 1,
-			"type": "spade"
-		},
-		"position10": {
-			"value": null,
-			"type": "wildCard"
-		}
-	},
-	{
-		"position1": {
-			"value": null,
-			"type": "wildCard"
-		},
-		"position2": {
-			"value": 8,
-			"cardType": "spade"
-		},
-		"position3": {
-			"value": 7,
-			"type": "spade"
-		},
-		"position4": {
-			"value": 6,
-			"type": "spade"
-		},
-		"position5": {
-			"value": 5,
-			"type": "spade"
-		},
-		"position6": {
-			"value": 4,
-			"type": "spade"
-		},
-		"position7": {
-			"value": 3,
-			"type": "spade"
-		},
-		"position8": {
-			"value": 2,
-			"type": "spade"
-		},
-		"position9": {
-			"value": 1,
-			"type": "spade"
-		},
-		"position10": {
-			"value": null,
-			"type": "wildCard"
-		}
-	},
-	{
-		"position1": {
-			"value": null,
-			"type": "wildCard"
-		},
-		"position2": {
-			"value": 8,
-			"cardType": "spade"
-		},
-		"position3": {
-			"value": 7,
-			"type": "spade"
-		},
-		"position4": {
-			"value": 6,
-			"type": "spade"
-		},
-		"position5": {
-			"value": 5,
-			"type": "spade"
-		},
-		"position6": {
-			"value": 4,
-			"type": "spade"
-		},
-		"position7": {
-			"value": 3,
-			"type": "spade"
-		},
-		"position8": {
-			"value": 2,
-			"type": "spade"
-		},
-		"position9": {
-			"value": 1,
-			"type": "spade"
-		},
-		"position10": {
-			"value": null,
-			"type": "wildCard"
-		}
-	},
-	{
-		"position1": {
-			"value": null,
-			"type": "wildCard"
-		},
-		"position2": {
-			"value": 8,
-			"cardType": "spade"
-		},
-		"position3": {
-			"value": 7,
-			"type": "spade"
-		},
-		"position4": {
-			"value": 6,
-			"type": "spade"
-		},
-		"position5": {
-			"value": 5,
-			"type": "spade"
-		},
-		"position6": {
-			"value": 4,
-			"type": "spade"
-		},
-		"position7": {
-			"value": 3,
-			"type": "spade"
-		},
-		"position8": {
-			"value": 2,
-			"type": "spade"
-		},
-		"position9": {
-			"value": 1,
-			"type": "spade"
+			"type": "diamond"
 		},
 		"position10": {
 			"value": null,

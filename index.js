000e1d3374e54ff22a554dda54ea1d997486bbff
dylanbrookes <33613317+dylanbrookes@users.noreply.github.com>
2019-02-11 02:21:31 -0500

    first attempt at getting random cards from deck assigned to player

--- index.js
+++ index.js
@@ -15,11 +15,59 @@ class Player {
     }
 }
 
-let playerArray = [
-    player1 = new Player(1),
-    player2 = new Player(2),
-    player3 = new Player(3),
-    player4 = new Player(4) 
-];
 
-console.log(playerArray[3]);
+
+class Deck {
+    constructor() {
+        let type = ["spade", "diamond", "club", "heart"];
+        let value = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"];
+        let deck = new Array();
+
+        for(var i = 0; i < type.length; i++)
+        {
+            for(var x = 0; x < value.length; x++)
+            {
+                var card = {Value: value[x], Suit: type[i]};
+                deck.push(card);
+            }
+        }
+
+        this.cards = deck;
+    }
+
+    assignPlayerCards() {
+        var tempArray = [];
+
+        for (var i = 0; i < 13; i++) {
+            var randomNumber = Math.floor(Math.random() * 53);
+            var tempCard = this.cards[randomNumber];
+    
+            // delete card from deck
+            this.cards.splice(randomNumber, 1);
+
+            tempArray.push(tempCard);
+        }
+
+        return tempArray;
+        
+    }
+}
+
+async function main() {
+
+    let playerArray = [
+        player1 = new Player(1),
+        player2 = new Player(2),
+        player3 = new Player(3),
+        player4 = new Player(4) 
+    ];
+    let gameDeck = await new Deck();
+    player1.cards = await gameDeck.assignPlayerCards();
+    console.log(player1);
+}
+
+
+main();
+
+
+

a52b3f336b2e538692cf49e67704a7b389e0412c
dylanbrookes <33613317+dylanbrookes@users.noreply.github.com>
2019-02-11 01:41:01 -0500

    created player list

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-for (var column = 0; column < data.length; column++) {
-    // get each column
-    for (var row = 0; row < data.length; row++) {
-        // get column's row value
-        if (data[column][row]['value'] === null)
-        console.log(data[column][row]);
+// for (var column = 0; column < data.length; column++) {
+//     // get each column
+//     for (var row = 0; row < data.length; row++) {
+//         // get column's row value
+//         if (data[column][row]['value'] === null)
+//         console.log(data[column][row]);
+//     }
+// }
+
+
+class Player {
+    constructor(id) {
+        this.id = id;
+        this.cards = null;
     }
 }
+
+let playerArray = [
+    player1 = new Player(1),
+    player2 = new Player(2),
+    player3 = new Player(3),
+    player4 = new Player(4) 
+];
+
+console.log(playerArray[3]);
